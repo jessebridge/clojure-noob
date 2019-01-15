911319d05badaee569e8ca4c0370a2a60264606d
jessebridge <jesse.bridge@my.jcu.edu.au>
2019-01-15 16:34:37 +1000

    Added some simple work with atoms to understand how they work

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -1,27 +1,46 @@
 (ns clojure-noob.core
-  (:gen-class))
+    (:gen-class))
 
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+      "I don't do a whole lot ... yet."
+      [& args]
+      (println "Hello, World!"))
+
+
 
 ;basic program takes in a name parameter and prints a greeting with the name
-(defn greet [name] (println(str "hello ", name)))
+(defn greet [name] (println (str "hello ", name)))
 (greet "bob")
 
 ;message shows how to have multiple if statements and an else
-(defn error-message [errorType] (println(str "Warning Code " (if (= errorType 1) "1 in action") (if (= errorType 2)"2 in action") (if (= errorType 3) "3 in action"))"No code available"))
+(defn error-message [errorType] (println (str "Warning Code " (if (= errorType 1) "1 in action") (if (= errorType 2) "2 in action") (if (= errorType 3) "3 in action")) "No code available"))
 
 ;Loop practice code, same as a while loop
-(defn loop-practice [number-of-loops] (loop [iteration 0] (println (str "iteration" iteration)) (if (> iteration number-of-loops) (println "Done")(recur (inc iteration)))))
+(defn loop-practice [number-of-loops] (loop [iteration 0] (println (str "iteration" iteration)) (if (> iteration number-of-loops) (println "Done") (recur (inc iteration)))))
+
+(def practice-atom (atom {:small-number 5
+                          :large-number 2001}))
+
+(defn increase-atom-values [atom-state increase-amount]
+      (merge-with + atom-state {:small-number increase-amount
+                                :large-number increase-amount}))
+
+
+
+
 
 ;prints several types of error messages when prompted
 (error-message 1)
 (error-message 2)
 (error-message 3)
 (error-message 5)
-;Does a loop 7 times
-(loop-practice 7)
+;Does a loop 4 times
+(loop-practice 4)
+
+(println "original atom" @practice-atom)
+(swap! practice-atom increase-atom-values 10)
+(println "current atom after 1 change" @practice-atom)
+(swap! practice-atom increase-atom-values 200)
+(println "current atom after 2 changes" @practice-atom)
 
